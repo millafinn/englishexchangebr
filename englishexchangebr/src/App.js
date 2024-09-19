8cbbab215129fdc63a189ad4212b1a6825e8943e
millafinn <milla_finn@hotmail.com>
2024-09-19 07:17:38 -0300

    Estrutura do Menu

---
+++ englishexchangebr/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <nav className="navbar">
+          <ul className="nav-links">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#courses">Sobre</a></li>
+            <li><a href="#services">Serviços</a></li>
+            <li><a href="#contact">Contatos</a></li>
+            <li><a href="#blog">Fórum</a></li>
+          </ul>          
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
